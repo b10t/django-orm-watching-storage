08dfa549f7bdb7193d3e2dc0b9039edb4781abb9
b10t <b10t.rus@gmail.com>
2022-01-29 20:14:30 +0800

    Добавлен модуль с функциями по визитам.

---
+++ visit_time_functions.py
@@ -0,0 +1,48 @@
+from django.utils import timezone
+from django.utils.timezone import localtime
+
+
+def is_visit_long(duration_visit, minutes=60):
+    """Определяет, длинный или нет визит.
+
+    Args:
+        duration_visit (timedelta): Продолжительность визита
+        minutes (int, optional): Лимит времени в минутах. Defaults to 60.
+
+    Returns:
+        boolean: Длинный или нет визит
+    """
+    if minutes <= duration_visit.seconds // 60:
+        return True
+    return False
+
+
+def get_duration(visit):
+    """Расчитывает продолжительность по времени визита.
+
+    Args:
+        visit (Visit): Запись из таблицы Visit
+
+    Returns:
+        timedelta: Разница во времени
+    """
+    leaved_at = timezone.now()
+
+    if leaved_at:
+        leaved_at = visit.leaved_at
+
+    return localtime(leaved_at) - localtime(visit.entered_at)
+
+
+def format_duration(duration_visit):
+    """Возвращает отформатированное время.
+
+    Args:
+        duration_visit (timedelta): Продолжительность по времени
+
+    Returns:
+        str: Отформатированное время
+    """
+    hours = duration_visit.seconds // 3600
+    minutes = duration_visit.seconds % 3600 // 60
+    return f'{hours}:{minutes}'
