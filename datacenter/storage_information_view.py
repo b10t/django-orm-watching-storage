efd527ee2728a551c4d217de74bb4dab7ede9df1
b10t <b10t.rus@gmail.com>
2022-01-29 20:17:12 +0800

    Отображаем людей, кто сейчас в хранилище.

--- datacenter/storage_information_view.py
+++ datacenter/storage_information_view.py
@@ -1,51 +1,24 @@
 from django.shortcuts import render
-from django.utils import timezone
-from django.utils.timezone import localtime
+from visit_time_functions import format_duration, get_duration, is_visit_long
 
-from datacenter.models import Passcard, Visit
-
-
-def get_duration(entered_at):
-    """Возвращает продолжительность времени.
-
-    Args:
-        entered_at (datetime): Дата и время входа
-
-    Returns:
-        str: Отформатированное время
-    """
-    return format_duration(localtime(timezone.now()) - localtime(entered_at))
-
-
-def format_duration(difference_time):
-    """Возвращает отформатированное время.
-
-    Args:
-        difference_time (timedelta): Разница во времени
-
-    Returns:
-        str: Отформатированное время
-    """
-    hours = difference_time.seconds // 3600
-    minutes = difference_time.seconds % 3600 // 60
-    return f'{hours}:{minutes}'
-
-
-def is_visit_long(visit, minutes=60):
-    # TODO пишите код здесь
-    return False
+from datacenter.models import Visit
 
 
 def storage_information_view(request):
+    """Отображает список людей, кто сейчас в хранилище."""
     non_closed_visits = []
 
     for visit in Visit.objects.filter(leaved_at=None):
-        non_closed_visits.append(dict(who_entered=visit.passcard.owner_name,
-                                      entered_at=visit.entered_at,
-                                      duration=get_duration(visit.entered_at))
-                                 )
+        non_closed_visits.append(
+            dict(
+                who_entered=visit.passcard.owner_name,
+                entered_at=visit.entered_at,
+                duration=format_duration(get_duration(visit)),
+                is_strange=is_visit_long(get_duration(visit), minutes=60)
+            )
+        )
 
     context = {
-        'non_closed_visits': non_closed_visits,  # не закрытые посещения
+        'non_closed_visits': non_closed_visits,
     }
     return render(request, 'storage_information.html', context)
