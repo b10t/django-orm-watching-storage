a62262929821bcb0bf90f0c9dd517e45b2777a0b
b10t <b10t.rus@gmail.com>
2022-02-03 09:27:41 +0800

    Использование основного вида кавычек.

--- project/settings.py
+++ project/settings.py
@@ -1,28 +1,22 @@
 import os
 
+import dj_database_url
 from environs import Env
 
 env = Env()
 env.read_env()
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'HOST': env.str('DB_HOST', ''),
-        'PORT': env.str('DB_PORT', '5434'),
-        'NAME': env.str('DB_NAME', ''),
-        'USER': env.str('DB_USER', ''),
-        'PASSWORD': env.str('DB_PASSWORD', ''),
-    }
+    'default': dj_database_url.parse(env.str('DB_URL', ''))
 }
 
 INSTALLED_APPS = ['datacenter']
 
 SECRET_KEY = os.getenv('SECRET_KEY', '')
 
-DEBUG = env.bool("DEBUG", False)
+DEBUG = env.bool('DEBUG', False)
 
-ROOT_URLCONF = "project.urls"
+ROOT_URLCONF = 'project.urls'
 
 ALLOWED_HOSTS = ['*']
 
